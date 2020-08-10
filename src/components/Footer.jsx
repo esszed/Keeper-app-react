1a4421461cf7988951ef1831f6922bf5812fa769
Štěpán Zoubek <stepan.zoubek@gmail.com>
2020-08-10 17:41:30 +0200

    App setup

---
+++ src/components/Footer.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const year = new Date().getFullYear()
+const Footer = ()=> <footer><p>© Štěpán Zoubek  {year}</p></footer>
+
+export default Footer
---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Note = () => {
+  return (
+    <div className='note'>
+      <h1>Some Note</h1>
+      <p>Title</p>
+    </div>
+  )
+}
+
+
+export default Note
