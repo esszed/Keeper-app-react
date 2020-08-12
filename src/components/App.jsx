0234a10dc475ea6b5f6c489764f27320f17a9b1d
Štěpán Zoubek <stepan.zoubek@gmail.com>
2020-08-13 00:03:01 +0200

    Add new task component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import CreateArea from './CreateArea'
 import Note from './Note'
 import notes from '../notes'
 const App = () => {
   return (
     <div>
       <Header />
+      <CreateArea />
       {notes.map(({ title, content }) => (
+        
          <Note title={title} content={content} />
       ))}
       <Footer />
