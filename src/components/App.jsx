5f867ff2c8467f0d856a1088c3306274e144c6f4
Štěpán Zoubek <stepan.zoubek@gmail.com>
2020-08-13 15:41:04 +0200

    Add notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import CreateArea from './CreateArea'
 import Note from './Note'
-import notes from '../notes'
+
 const App = () => {
+  const [notesCollection, setNotesCol] = useState([{title:'Note', content:'bang'}])
+
+  const addNote = (note) => {
+
+    setNotesCol([...notesCollection, note])
+  
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      {notes.map(({ title, content }) => (
-        
-         <Note title={title} content={content} />
+      <CreateArea onAdd={addNote} />
+      {notesCollection.map(({ title, content },index) => (
+        <Note key={index}  title={title} content={content} />
       ))}
       <Footer />
     </div>
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from 'react'
 
-function CreateArea() {
+const CreateArea = ({onAdd}) => {
+  
+  const [note, setNote] = useState({
+    title: '',
+    content: ''
+  })
+  
+  const handleInput = ({ target }) => {
+    const { name, value } = target
+    setNote(prevValues => ({ ...prevValues, [name]: value }))
+  }
+
+  const handleClick=(e)=>{
+    onAdd(note)
+    e.preventDefault()
+  }
+  // const { name, text } = note
   return (
+    
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
-      </form>
+        <form>
+        <input onChange={handleInput} name='title' placeholder='Title' />
+        <textarea onChange={handleInput}  name='content' placeholder='Take a note...' rows='3' />
+        <button onClick={handleClick}>Add</button>
+        </form>
     </div>
-  );
+  )
 }
 
-export default CreateArea;
+export default CreateArea
